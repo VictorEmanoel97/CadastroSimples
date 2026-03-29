224aab960aeafcc7379fbd82cd9a2423aaa347ad
VictorEmanoel97 <oliveirapreto45@gmail.com>
2026-03-29 17:21:46 -0300

    validação concluida

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const caixatexto = document.getElementsByClassName("caixa");
-let limp = document.getElementById("limpar");
-function limpar() {
-    caixatexto.value = "";
+let caixatexto = document.getElementsByClassName("caixa");
+const limp = document.getElementById("limpar");
+
+const enviarr = document.getElementById("enviar");
+
+function validar() {
+    for (let i = 0; i < caixatexto.length; i++) {
+        if (caixatexto[i].value == "") {
+            alert("preencha o campo");
+            return;
+        }
+
+   }
+    alert("cadastro realizado com sucesso");
 }
